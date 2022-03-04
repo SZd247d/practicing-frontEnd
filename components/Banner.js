753d12f9f246cf78a2894b51261af271dba74b32
zakisellali <zakisellali464@gmail.com>
2022-03-04 18:58:54 +0100

    most frontEnd completed

---
+++ components/Banner.js
@@ -0,0 +1,33 @@
+import 'react-responsive-carousel/lib/styles/carousel.min.css'
+import { Carousel } from 'react-responsive-carousel'
+
+function Banner() {
+  return (
+    <div className="relative ">
+      <div className="absolute bottom-0 z-20 h-32 w-full bg-gradient-to-t from-gray-100 to-transparent" />
+
+      <Carousel
+        autoPlay
+        infiniteLoop
+        showStatus={false}
+        showIndicators={false}
+        showThumbs={false}
+        interval={5000}
+      >
+        <div>
+          <img src="http://links.papareact.com/gi1" loading="lazy" alt="" />
+        </div>
+
+        <div>
+          <img src="http://links.papareact.com/6ff" loading="lazy" alt="" />
+        </div>
+
+        <div>
+          <img src="http://links.papareact.com/7ma" loading="lazy" alt="" />
+        </div>
+      </Carousel>
+    </div>
+  )
+}
+
+export default Banner
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,38 @@
 import Head from 'next/head'
+import Banner from '../components/banner'
 import Header from '../components/Header'
+import ProductFeed from '../components/ProductFeed'
 
-const Home = () => {
+const Home = ({ products }) => {
+  console.log(products)
   return (
-    <div className="h-screen bg-slate-600">
+    <div className="min-h-screen bg-slate-300">
       <Head>
-        <title>Create Next App</title>
+        <title>AmazonClone</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      {/* Header */}
       <Header />
+
+      <main className="mx-auto max-w-screen-2xl">
+        <Banner />
+
+        <ProductFeed products={products} />
+      </main>
     </div>
   )
 }
 
 export default Home
+
+export async function getServerSideProps(context) {
+  const products = await fetch('https://fakestoreapi.com/products').then(
+    (res) => res.json()
+  )
+
+  return {
+    props: {
+      products,
+    },
+  }
+}
